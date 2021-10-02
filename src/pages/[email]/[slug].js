271afe87af2e5105cd7c8f10d90ecdbbec809159
Nguyen Dinh Tuan Anh <ndtatuananh@gmail.com>
2021-10-02 18:50:21 +0700

    finish detail page

--- src/pages/[email]/[slug].js
+++ src/pages/[email]/[slug].js
@@ -4,32 +4,32 @@ import {
   CircularProgress,
   Container,
   Grid,
-} from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
 import {
   fetchNewestTheses,
   fetchThesisBySlug,
-} from 'modules/theses/fetch-theses'
-import PdfViewer from 'modules/theses/pdf/PdfViewer'
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+} from "modules/theses/fetch-theses";
+import PdfViewer from "modules/theses/pdf/PdfViewer";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 export async function getStaticPaths() {
-  const data = await fetchNewestTheses(process.env.API_URL)
+  const data = await fetchNewestTheses(process.env.API_URL);
 
-  const paths = data.map(item => {
+  const paths = data.map((item) => {
     return {
       params: {
         email: item.user.email,
         slug: item.slug,
       },
-    }
-  })
+    };
+  });
 
   return {
     paths,
     fallback: true,
-  }
+  };
 }
 
 export async function getStaticProps({ params }) {
@@ -37,12 +37,32 @@ export async function getStaticProps({ params }) {
     props: {
       details: await fetchThesisBySlug(process.env.API_URL, params.slug),
     },
-  }
+  };
 }
 
 export default function ThesisDetail({ details }) {
-  const classes = useStyle()
-  const router = useRouter()
+  const classes = useStyle();
+  const router = useRouter();
+
+  const gridItemProperty = {
+    property: {
+      item: true,
+      xl: 3,
+      xs: 3,
+      sm: 3,
+      md: 3,
+    },
+
+    value: {
+      item: true,
+      xl: 9,
+      xs: 9,
+      sm: 9,
+      md: 9,
+    },
+  };
+
+  const email = "18520093@gm.uit.edu.vn";
 
   // cho trang fetch du lieu -> hien thi component loading
   if (router.isFallback) {
@@ -56,7 +76,7 @@ export default function ThesisDetail({ details }) {
           <CircularProgress size={50} />
         </Box>
       </>
-    )
+    );
   }
 
   return (
@@ -66,70 +86,91 @@ export default function ThesisDetail({ details }) {
       </Head>
 
       <h1>Chi tiết luận văn</h1>
-      {/* <pre>{JSON.stringify(details, 0, 2)}</pre> */}
       <Container maxWidth="md" className={classes.container}>
-        <Grid container className={classes.gridContainer}>
-          <Grid item xl={2} xs={3} sm={3} md={2}>
-            Loại tài liệu:
-          </Grid>
-          <Grid item xl={10} xs={9} sm={9} md={10}>
-            {details.type}
-          </Grid>
-        </Grid>
-        <Grid container className={classes.gridContainer}>
-          <Grid item xl={2} xs={3} sm={3} md={2}>
+        <Grid container>
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
             Tên:
           </Grid>
-          <Grid item xl={10} xs={9} sm={9} md={10}>
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
             {details.name}
           </Grid>
-        </Grid>
-        <Grid container className={classes.gridContainer}>
-          <Grid item xl={2} xs={3} sm={3} md={2}>
-            Khoa:
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
+            Loại:
           </Grid>
-          <Grid item xl={10} xs={9} sm={9} md={10}>
-            {details.faculty}
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
+            {details.type}
           </Grid>
-        </Grid>
-        <Grid container className={classes.gridContainer}>
-          <Grid item xl={2} xs={3} sm={3} md={2}>
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
+            Tác giả:
+          </Grid>
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
+            {details.author.map((name) => (
+              <div>{name}</div>
+            ))}
+          </Grid>
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
+            GVHD:
+          </Grid>
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
+            {details.teachers.map((teacher) => (
+              <div>{teacher}</div>
+            ))}
+          </Grid>
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
             Năm xuất bản:
           </Grid>
-          <Grid item xl={10} xs={9} sm={9} md={10}>
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
             {details.published_year}
           </Grid>
-        </Grid>
-        <Grid container className={classes.gridContainer}>
-          <Grid item xl={2} xs={3} sm={3} md={2}>
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
+            Định dạng:
+          </Grid>
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
+            {details.format.map((f) => (
+              <div>{f}</div>
+            ))}
+          </Grid>
+
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
             Ngôn ngữ:
           </Grid>
-          <Grid item xl={10} xs={9} sm={9} md={10}>
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
             {details.language}
           </Grid>
-        </Grid>
-        <Grid container className={classes.gridContainer}>
-          <Grid item xl={2} xs={3} sm={3} md={2}>
+
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
             Lượt xem:
           </Grid>
-          <Grid item xl={10} xs={9} sm={9} md={10}>
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
             {details.views}
+          </Grid>
+          <Grid {...gridItemProperty.property} className={classes.gridItem}>
+            Thẻ:
+          </Grid>
+          <Grid {...gridItemProperty.value} className={classes.gridItem}>
+            {details.tags.map((item) => (
+              <div>{item}</div>
+            ))}
           </Grid>
         </Grid>
         <Box display="flex" justifyContent="flex-end" mt={2}>
-          <Button variant="contained" color="primary">
+          <Button
+            disabled={details.user.email !== email}
+            variant="contained"
+            color="primary"
+          >
             Chỉnh sửa
           </Button>
         </Box>
       </Container>
       <PdfViewer file={details.link_storage} isList={false} />
     </>
-  )
+  );
 }
 
-const useStyle = makeStyles(theme => ({
-  gridContainer: {
-    padding: '1rem',
-    borderBottom: '1px solid #ddd',
+const useStyle = makeStyles((theme) => ({
+  gridItem: {
+    padding: "1rem",
+    borderBottom: "1px solid #ddd",
   },
-}))
+}));
