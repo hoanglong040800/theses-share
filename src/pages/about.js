618562b020637b59ed52882ad0c16b3a71608320
Nguyen Dinh Tuan Anh <ndtatuananh@gmail.com>
2022-01-15 22:57:59 +0700

    finish about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { makeStyles } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
 
 export default function About() {
   const mui = useStyles();
@@ -11,8 +11,8 @@ export default function About() {
         <title>Về Theses Share</title>
       </Head>
 
-      <div className={mui.about_section}>
-        <div className={mui.about_image}>
+      <Grid container>
+        <Grid item xs={12} lg={6} className={mui.image}>
           <Image
             width={200}
             height={200}
@@ -20,10 +20,10 @@ export default function About() {
             src="/b_5d09b19471c58.svg"
             alt="thesis image"
           />
-        </div>
-        <div className={mui.paragraph}>
+        </Grid>
+        <Grid item xs={12} lg={6} className={mui.text_section}>
           <h2 className={mui.heading}>Về Theses Share</h2>
-          <p>
+          <p className={mui.paragraph}>
             Ứng dụng Theses Share ra đời để sinh viên UIT có thể chia sẻ khóa
             luận của mình cho mọi sinh viên tham khảo. Việc có nhiều nguồn tài
             liệu khóa luận sẽ giúp những đề tài khóa sau càng đa dạng hơn và
@@ -31,7 +31,10 @@ export default function About() {
             phân loại tags các đề tài để giúp sinh viên tìm kiếm khóa luận theo
             những chủ đề mình mong muốn một cách dễ dàng hơn.
           </p>
-          <h2 className={mui.heading}>Mục tiêu</h2>
+          <br />
+          <hr className={mui.line_break} />
+          <br />
+          <h2 className={mui.heading}>Vì sao chọn Theses Share?</h2>
           <ul className={mui.list}>
             <li>
               Cho phép sinh viên xem đầy đủ báo cáo khóa luận dạng PDF cùng với
@@ -58,36 +61,141 @@ export default function About() {
               tăng cao trải nghiệm người dùng
             </li>
           </ul>
-        </div>
-      </div>
+        </Grid>
+
+        <Grid item xs={12}>
+          <br />
+          <hr className={mui.line_break} />
+          <br />
+          <h2 className={mui.heading}>Các thành viên</h2>
+          <Grid container className={mui.team_container}>
+            <Grid item xs={12} sm={6} lg={3} className={mui.member}>
+              <a
+                href="https://github.com/hoanglong040800"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  layout="responsive"
+                  src="/long.png"
+                  alt="long"
+                  className={mui.team_image}
+                />
+                <p>Trần Hoàng Long</p>
+              </a>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={3} className={mui.member}>
+              <a
+                href="https://github.com/ueihvn"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  layout="responsive"
+                  src="/hieu.png"
+                  alt="hieu"
+                  className={mui.team_image}
+                />
+                <p>Lê Trọng Hiếu</p>
+              </a>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={3} className={mui.member}>
+              <a
+                href="https://github.com/nhoxlove2k15"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  layout="responsive"
+                  src="/duy.png"
+                  alt="duy"
+                  className={mui.team_image}
+                />
+                <p>Đôn Khánh Duy</p>
+              </a>
+            </Grid>
+            <Grid item xs={12} sm={6} lg={3} className={mui.member}>
+              <a
+                href="https://github.com/nhoxbinndta"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <Image
+                  width={100}
+                  height={100}
+                  layout="responsive"
+                  src="/anh.jpg"
+                  alt="anh"
+                  className={mui.team_image}
+                />
+                <p>Nguyễn Đình Tuấn Anh</p>
+              </a>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   heading: {
-    opacity: 0.7,
     fontSize: "2rem",
     fontWeight: "normal",
-    paddingTop: 30,
+    opacity: 0.8,
     textAlign: "center",
   },
 
-  about_section: {
-    display: "flex",
-  },
-
-  about_image: {
-    flex: 1,
+  text_section: {
+    fontSize: "1.2rem",
   },
 
   paragraph: {
-    fontSize: "1rem",
-    flex: 1,
+    lineHeight: 1.6,
+    opacity: 0.8,
   },
 
   list: {
-    listStyleType: "none",
-    padding: 0,
+    listStyleType: "square",
+    paddingLeft: 20,
+    opacity: 0.8,
+    "& li": {
+      marginTop: 10,
+    },
   },
-});
+
+  team_container: {
+    "& *": {
+      textAlign: "center",
+    },
+  },
+
+  member: {
+    padding: 50,
+    "& p": {
+      fontSize: "1.2rem",
+    },
+  },
+
+  team_image: {
+    paddingBottom: 10,
+    borderRadius: "50%",
+  },
+
+  line_break: {
+    opacity: 0.3,
+    width: "75%",
+  },
+
+  [theme.breakpoints.down("md")]: {
+    image: {
+      display: "none",
+    },
+  },
+}));
