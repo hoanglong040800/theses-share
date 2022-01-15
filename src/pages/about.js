8be7594d49442290b78eae5ad9b0777180d8b852
Nguyen Dinh Tuan Anh <ndtatuananh@gmail.com>
2022-01-15 20:14:40 +0700

    about us description

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/client";
+import Image from "next/image";
+import { makeStyles } from "@material-ui/core";
 
 export default function About() {
-  const [session, loading] = useSession();
+  const mui = useStyles();
 
   return (
     <>
@@ -10,24 +11,83 @@ export default function About() {
         <title>Về Theses Share</title>
       </Head>
 
-      <h1>Về Theses Share</h1>
-
-      {!session && (
-        <>
-          <h3>You are not signed in</h3>
-          <br />
-          <button onClick={signIn}>Sign in</button>
-        </>
-      )}
-
-      {session && (
-        <>
-          <h3>Signed in as {session.user.full_name} </h3>
-          <pre>{JSON.stringify(session, null, 2)}</pre>
-          <br />
-          <button onClick={signOut}>Sign out</button>
-        </>
-      )}
+      <div className={mui.about_section}>
+        <div className={mui.about_image}>
+          <Image
+            width={200}
+            height={200}
+            layout="responsive"
+            src="/b_5d09b19471c58.svg"
+            alt="thesis image"
+          />
+        </div>
+        <div className={mui.paragraph}>
+          <h2 className={mui.heading}>Về Theses Share</h2>
+          <p>
+            Ứng dụng Theses Share ra đời để sinh viên UIT có thể chia sẻ khóa
+            luận của mình cho mọi sinh viên tham khảo. Việc có nhiều nguồn tài
+            liệu khóa luận sẽ giúp những đề tài khóa sau càng đa dạng hơn và
+            chỉn chu hơn. Ngoài việc chia sẻ, ứng dụng còn chú trọng vào việc
+            phân loại tags các đề tài để giúp sinh viên tìm kiếm khóa luận theo
+            những chủ đề mình mong muốn một cách dễ dàng hơn.
+          </p>
+          <h2 className={mui.heading}>Mục tiêu</h2>
+          <ul className={mui.list}>
+            <li>
+              Cho phép sinh viên xem đầy đủ báo cáo khóa luận dạng PDF cùng với
+              những thông tin miêu tả
+            </li>
+            <li>
+              Cung cấp bộ tìm kiếm nâng cao giúp sinh viên tìm được những chủ đề
+              mình hướng đến
+            </li>
+            <li>
+              Cung cấp các chức năng đăng ký, đăng nhập, đăng tải luận văn dạng
+              PDF đi kèm với thông tin, mô tả, ...
+            </li>
+            <li>
+              Chức năng yêu thích để sinh viên có thể lưu lại những luận văn để
+              tiện hơn cho những lần đọc sau
+            </li>
+            <li>
+              Giao diện đơn giản, trực quan, tương thích với nhiều kích cỡ màn
+              hình
+            </li>
+            <li>
+              Tốc độ load trang web nhanh và mọi thao tác đều nằm trên 1 trang,
+              tăng cao trải nghiệm người dùng
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
+
+const useStyles = makeStyles({
+  heading: {
+    opacity: 0.7,
+    fontSize: "2rem",
+    fontWeight: "normal",
+    paddingTop: 30,
+    textAlign: "center",
+  },
+
+  about_section: {
+    display: "flex",
+  },
+
+  about_image: {
+    flex: 1,
+  },
+
+  paragraph: {
+    fontSize: "1rem",
+    flex: 1,
+  },
+
+  list: {
+    listStyleType: "none",
+    padding: 0,
+  },
+});
